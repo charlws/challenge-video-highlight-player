b69bcdcaa301d6872c9975bd2e7beefe9e0fe331
CharlesWithC <charles_1414@outlook.com>
2024-09-29 20:13:34 +0000

    nextjs init repo

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Video Player with Highlights</title>
+      </Head>
+      <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        Hello World
+      </div>
+    </>
+  );
+}
